13792a6bfc3b3b94a9f4317a9353cd2e124cfd3c
Tsion <tsionwendmu12@gmail.com>
2023-09-09 13:57:20 -0400

    s

--- markdown/src/components/Header.jsx
+++ markdown/src/components/Header.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
+
 export default function Header () {
+  const [toggle, setToggle] = useState()
+
+  const toggleFunction = () => {
+    console.log('hey')
+    setToggle(!toggle)
+  }
     return(
   <div>
     <header>
         <h1>Markdown Previewer</h1>
-        <button>Markdown Cheat Sheet</button>
+        <button onClick={toggleFunction}>Markdown Cheat Sheet</button>
     </header>
+
+    <div className="toggleBox">
+        <h1>Markdown Cheat Sheet</h1>
+        <ul>
+            <li># H1</li>
+            <li>## H2</li>
+            <li>### H3</li>
+            <li>**bold**</li>
+            <li>*italic*</li>
+            <li>[Link](http://a.com)</li>
+            <li>![Image](http://url/a.png)</li>
+            <li>`inline code`</li>
+            <li>```block code```</li>
+            <li>- list item</li>
+        </ul>
+    </div>
 
     </div>  
       
