ffeaecdf38915ccdf0b5a51ca99da56ed4fddbae
Tsion <tsionwendmu12@gmail.com>
2023-09-09 20:43:51 -0400

    s

--- markdown/src/components/MarkdownInput.jsx
+++ markdown/src/components/MarkdownInput.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function MarkdownInput() {
-    const InputContainer = styled.div`
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
+const InputContainer = styled.div`
+width: 100%;
+max-width: 600px;
+margin: 0 auto;
+padding: 20px;
 `;
 
 // Define StyledTextArea styled-component with CSS for the textarea
 const StyledTextArea = styled.textarea`
-  width: 100%;
-  height: 300px;
-  border: 1px solid #ccc;
-  padding: 10px;
-  font-size: 16px;
-  resize: vertical;
-  box-sizing: border-box;
+width: 100%;
+height: 300px;
+border: 1px solid #ccc;
+padding: 10px;
+font-size: 16px;
+resize: vertical;
+box-sizing: border-box;
 `
+
+export default function MarkdownInput(props) {
+
     return(
         <div>
     <InputContainer>
-      <StyledTextArea placeholder="Type your Markdown here..." />
+      <StyledTextArea placeholder="Type your Markdown here..."
+                      value={props.value}
+                      onChange={props.onChange}/>
     </InputContainer>
         </div>
     )
--- markdown/src/components/MarkdownOutput.jsx
+++ markdown/src/components/MarkdownOutput.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+import styled from 'styled-components'
+
+const OutputContainer = styled.div`
+width: 100%;
+max-width: 600px;
+margin: 0 auto;
+padding: 20px;
+background-color: #f7f7f7; /* Example background color */
+border: 1px solid #ccc; /* Example border */
+border-radius: 4px; /* Example border radius */
+`;
+
 export default function MarkdownOutput() {
+
+
     return(
-        <div>
-            <h1>preview</h1>
-        </div>
+        <OutputContainer>
+        <div dangerouslySetInnerHTML={{ __html: markdown }} />
+      </OutputContainer>
     )
 }
